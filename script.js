b7078643170add9bad9211652a80f4e0db0f9181
boss10211 <boss10211@proton.me>
2024-09-07 15:14:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -43,8 +43,8 @@ function generatePromoCode() {
 }
 
 function sendTelegramMessage(reward, promoCode) {
-    const adminChatId = 'YOUR_ADMIN_CHAT_ID';
-    const botToken = 'YOUR_BOT_TOKEN';
+    const adminChatId = '7234287467:AAGaT2z1qI-rdIf2RzTZmJRrqnqK5z4pJb4';
+    const botToken = '5920944588';
     const message = `Игрок выиграл ${reward}. Промокод: ${promoCode}`;
     fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${adminChatId}&text=${encodeURIComponent(message)}`);
 }
