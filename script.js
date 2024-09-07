5b8e62c6f7775b08bcef3424f67c85a333cb04f4
boss10211 <boss10211@proton.me>
2024-09-07 12:17:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,27 +16,3 @@ document.getElementById('coin').addEventListener('click', () => {
 document.getElementById('winCoinsButton').addEventListener('click', () => {
     window.location.href = 'caif coin game.html'; // Путь к вашей другой игре
 });
-
-function checkPrizes(score) {
-    let message = '';
-    if (score = 1200) {
-        message = 'Поздравляем! Вы выиграли 1 грамм золота!';
-    } else if (score = 1150) {
-        message = 'Поздравляем! Вы выиграли 0.7 грамма золота!';
-    } else if (score = 1100) {
-        message = 'Поздравляем! Вы выиграли 0.5 грамма золота!';
-    } else if (score = 1050) {
-        message = 'Поздравляем! Вы получили 50% скидку!';
-    } else if (score = 1000) {
-        message = 'Поздравляем! Вы получили 25% скидку!';
-    } else if (score = 950) {
-        message = 'Поздравляем! Вы получили 20% скидку!';
-    } else if (score = 900) {
-        message = 'Поздравляем! Вы получили 10% скидку!';
-    } else if (score = 850) {
-        message = 'Поздравляем! Вы получили 5% скидку!';
-    }
-    if (message) {
-        alert(message);
-    }
-}
