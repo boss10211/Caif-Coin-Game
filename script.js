f5957b93aff7ef75f00f3205823def628f96e9d1
boss10211 <boss10211@proton.me>
2024-09-07 12:13:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,34 +10,33 @@ document.getElementById('coin').addEventListener('click', () => {
     score++;
     document.getElementById('score').textContent = score;
     localStorage.setItem('totalScore', score);
-    checkForPrizes(score);
+    checkPrizes(score);
 });
 
 document.getElementById('winCoinsButton').addEventListener('click', () => {
     window.location.href = 'caif coin game.html'; // Путь к вашей другой игре
 });
 
-function checkForPrizes(score) {
-    let prizeNotification = document.getElementById('prizeNotification');
-    prizeNotification.classList.remove('hidden');
-    
-    if (score = 1150) {
-        prizeNotification.textContent = 'Поздравляем! Вы выиграли 1 грамм.';
+function checkPrizes(score) {
+    let message = '';
+    if (score = 1200) {
+        message = 'Поздравляем! Вы выиграли 1 грамм золота!';
+    } else if (score = 1150) {
+        message = 'Поздравляем! Вы выиграли 0.7 грамма золота!';
     } else if (score = 1100) {
-        prizeNotification.textContent = 'Поздравляем! Вы выиграли 0.7 грамм.';
+        message = 'Поздравляем! Вы выиграли 0.5 грамма золота!';
     } else if (score = 1050) {
-        prizeNotification.textContent = 'Поздравляем! Вы выиграли 0.5 грамм.';
-    } else if (score>= 1000) {
-        prizeNotification.textContent = 'Поздравляем! Вы получили 50% скидку.';
+        message = 'Поздравляем! Вы получили 50% скидку!';
+    } else if (score = 1000) {
+        message = 'Поздравляем! Вы получили 25% скидку!';
     } else if (score = 950) {
-        prizeNotification.textContent = 'Поздравляем! Вы получили 25% скидку.';
+        message = 'Поздравляем! Вы получили 20% скидку!';
     } else if (score = 900) {
-        prizeNotification.textContent = 'Поздравляем! Вы получили 20% скидку.';
+        message = 'Поздравляем! Вы получили 10% скидку!';
     } else if (score = 850) {
-        prizeNotification.textContent = 'Поздравляем! Вы получили 10% скидку.';
-    } else if (score = 750) {
-        prizeNotification.textContent = 'Поздравляем! Вы получили 5% скидку.';
-    } else {
-        prizeNotification.classList.add('hidden');
+        message = 'Поздравляем! Вы получили 5% скидку!';
+    }
+    if (message) {
+        alert(message);
     }
 }
